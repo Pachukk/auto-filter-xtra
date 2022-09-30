c807485b9df58e34299e125ffbea1c39c640b0c7
PachGit <108862443+PachGit@users.noreply.github.com>
2022-09-30 12:00:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Aadhi000/Ajax.git /Ajax
+  git clone https://github.com/PachGit/auto-filter-xtra.git /Aauto-filter-xtra
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ajax
+  git clone $UPSTREAM_REPO /auto-filter-xtra
 fi
-cd /Ajax
+cd /auto-filter-xtra
 pip3 install -U -r requirements.txt
-echo "Starting ᗩᒍᗩ᙭....🔥"
+echo "Starting 𝘾𝙝𝙖𝙧𝙡𝙞𝙚 𝘾𝙝𝙖𝙥𝙡𝙞𝙣...🔥"
 python3 bot.py
